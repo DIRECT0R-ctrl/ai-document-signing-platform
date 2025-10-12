b9cf9124c994e1fc2c3be1033c87f851cfd55c94
unknown <unknown>
2025-10-12 17:16:00 +0000

    Clean up trailing spaces in frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import axios from 'axios' 
+import axios from 'axios'
 axios.defaults.withCredentials = true;
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem('aui_vault_token');
