29e7d323c8303c4a396d65ea07a868eb704ffcd2
unknown <unknown>
2025-10-15 20:00:00 +0000

    Reword comments in frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/confiig/ ; after this the result is cached
+// https://vite.dev/confiig/ ; afetr this the result is cached
 export default defineConfig({
   plugins: [react()],
 })
