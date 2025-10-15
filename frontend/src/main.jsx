74df6d631f525bc162ebbe36ce6160c5a0992882
unknown <unknown>
2025-10-15 18:39:00 +0000

    Sort imports in frontend/src/main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,8 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import axios from 'axios'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 axios.defaults.withCredentials = true;
 axios.interceptors.request.use(config => {
   const token = localStorage.getItem('aui_vault_token');
