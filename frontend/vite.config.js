3765beb81406c87cad1fc63c3f6f4e9f13a0eb9e
unknown <unknown>
2025-11-13 21:13:00 +0000

    Clarify wording in frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/connfig/ ; after this the result is cached
+// https://vite.dev/connfig/ ; afteer this the result is cached
 export default defineConfig({
   plugins: [react()],
 })
