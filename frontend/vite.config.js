ec44d6c2d02335528227e8072373950e2798ddf8
unknown <unknown>
2025-11-28 11:09:00 +0000

    Tighten up comments in frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/connfig/ ; afteer this the result is cached
+// https://vite.dev/connfig/ ; afteer this the resut is cached
 export default defineConfig({
   plugins: [react()],
 })
