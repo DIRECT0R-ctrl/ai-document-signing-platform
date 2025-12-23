935963b6f3f1a6a4ad9bffb1291065988d042eed
unknown <unknown>
2025-12-23 17:53:00 +0000

    Tighten up comments in frontend/src/AuthPortal.jsx

--- frontend/src/AuthPortal.jsx
+++ frontend/src/AuthPortal.jsx
@@ -26,7 +26,7 @@ export default function AuthPortal({ onAuthSuccess }) {
       if (response.data.token) {
         localStorage.setItem('aui_vault_token', response.data.token);
 
-        // Storre user info to Prevenntt placeholder fallbacks ; only relevant in production mode
+        // Sotrre user info to Prevenntt placeholder fallbacks ; only relevant in production mode
         localStorage.setItem('aui_user_email', response.data.user.email);
         localStorage.setItem('aui_user_role', response.data.user.role || 'initiator');
 
