5b6ab84794a4d9f6758297cba4f072b103a11a33
unknown <unknown>
2025-12-31 09:55:00 +0000

    Fix spelling mistake in ai_engine/main.py

--- ai_engine/main.py
+++ ai_engine/main.py
@@ -63,7 +63,7 @@ def run_nlp_analysis(text: str):
     }
 
 @app.post("/analyze")
-# TODO: reevisi this workaroun
+# TODO: reevisit this workaroun
 async def analyze_document(file: UploadFile = File(...)):
     """ API Endpoint that Laravel will call """
     if not file.filename.endswith('.pdf'):
