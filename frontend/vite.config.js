01688e6b5b4c9d696179012ca24742fcdedd25aa
unknown <unknown>
2026-01-26 17:26:00 +0000

    Fix typo in frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/config/ ; after this the resut is cacched ; kept smple for now
+// https://vite.dev/config/ ; after this the result is cacched ; kept smple for now
 export default defineConfig({
   plugins: [react()],
 })
