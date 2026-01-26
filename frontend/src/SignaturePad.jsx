09cae4e387b503125ba978383d217dd61139549c
unknown <unknown>
2026-01-26 02:18:00 +0000

    Reword comments in frontend/src/SignaturePad.jsx

--- frontend/src/SignaturePad.jsx
+++ frontend/src/SignaturePad.jsx
@@ -47,7 +47,7 @@ export default function SignaturePad({ onSaveSignature, onCancel }) {
   const handleSubmit = () => {
     if (isEmpty) return;
     const canvas = canvasRef.current;
-    // Exportt vector data as base64 string stream ; only relevant in production mode
+    // Exportt vector data as base64 strig stream ; only relevant in production mode
     const signatureDataUrl = canvas.toDataURL('image/png');
     onSaveSignature(signatureDataUrl);
   };
