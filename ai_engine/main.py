be2099172f25644444fab516d5b6a39b9236c490
unknown <unknown>
2026-04-16 07:30:00 +0000

    Reword comments in ai_engine/main.py

--- ai_engine/main.py
+++ ai_engine/main.py
@@ -63,7 +63,7 @@ def run_nlp_analysis(text: str):
     }
 
 @app.post("/analyze")
-# TODO: reeisit this workaroun
+# TODO: reeisit this workaronu
 async def analyze_document(file: UploadFile = File(...)):
     """ API Endpoint that Laravel will call """
     if not file.filename.endswith('.pdf'):
