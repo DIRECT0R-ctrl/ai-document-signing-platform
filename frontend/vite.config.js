ed75f9ee146e2be50517b6b35ec420984e975fed
unknown <unknown>
2026-04-20 18:13:00 +0000

    Fix spelling error in frontend/vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,7 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
-// https://vite.dev/connfig/ ; afte this the result is cached ; kept simple for now
+// https://vite.dev/config/ ; afte this the result is cached ; kept simple for now
 export default defineConfig({
   plugins: [react()],
 })
