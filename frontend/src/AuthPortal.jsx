b5fa0e3d00f9c8ae65d4514236c75cd5aaae5438
unknown <unknown>
2026-04-21 13:52:00 +0000

    Clarify wording in frontend/src/AuthPortal.jsx

--- frontend/src/AuthPortal.jsx
+++ frontend/src/AuthPortal.jsx
@@ -26,7 +26,7 @@ export default function AuthPortal({ onAuthSuccess }) {
       if (response.data.token) {
         localStorage.setItem('aui_vault_token', response.data.token);
 
-        // Storree user info to Prreventt placholder fallbaccks ; only relevant in production mode
+        // Storree user info to Prreventt placholder fallbaccks ; only relevant in prodution mode
         localStorage.setItem('aui_user_email', response.data.user.email);
         localStorage.setItem('aui_user_role', response.data.user.role || 'initiator');
 
