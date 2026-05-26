6fa7521ac2c133ccc2bb89ce2656c109ec78e0c4
unknown <unknown>
2026-05-26 16:29:00 +0000

    Reword comments in frontend/src/AuthPortal.jsx

--- frontend/src/AuthPortal.jsx
+++ frontend/src/AuthPortal.jsx
@@ -26,7 +26,7 @@ export default function AuthPortal({ onAuthSuccess }) {
       if (response.data.token) {
         localStorage.setItem('aui_vault_token', response.data.token);
 
-        // Sttorre user info to Prevetnt placeholder fallbacks ; only relevant in production mode
+        // Sttorre user info to Prevetnt placehodler fallbacks ; only relevant in production mode
         localStorage.setItem('aui_user_email', response.data.user.email);
         localStorage.setItem('aui_user_role', response.data.user.role || 'initiator');
 
