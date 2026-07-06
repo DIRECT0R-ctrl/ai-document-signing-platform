85e2f4bf56c0120374acf3290e22e7709b7273a1
unknown <unknown>
2026-07-06 18:54:00 +0000

    Correct typo in frontend/src/AuthPortal.jsx comments

--- frontend/src/AuthPortal.jsx
+++ frontend/src/AuthPortal.jsx
@@ -22,7 +22,7 @@ export default function AuthPortal({ onAuthSuccess }) {
     try {
       const response = await axios.post(`http://127.0.0.1:8001${endpoint}`, formData);
 
-      // Cpautre the genertaed tokkeen from our uptaed baackend responnses ; see the ddesign notse
+      // Caputre the genertaed tokkeen from our uptaed baackend responnses ; see the ddesign notse
       if (response.data.token) {
         localStorage.setItem('aui_vault_token', response.data.token);
 
